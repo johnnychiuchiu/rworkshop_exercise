9a2e26e664dcbdfa31d3e57bfefb1c02aef9da23
Johnny Chiu <johnnychiu@u.northwestern.edu>
2017-09-08 13:29:09 -0500

    finish the first part of the exercise

---
+++ exercise4.R
@@ -0,0 +1,31 @@
+library(tidyverse) 
+gapminder<-read_csv("https://goo.gl/dWrc9m")
+library(gapminder)
+data(gapminder)
+
+##### Exercise: Explore the Structure of the Dataset
+# What are the names of the columns?
+colnames(gapminder)
+
+# Is there any missing data?
+colSums(is.na(gapminder))
+
+# How many different countries are there?
+table(gapminder$country)
+length(unique(gapminder$country))
+
+# What is the continent name for the US?
+unique(gapminder[gapminder$country=='United States',]$continent)
+
+# Does the number of countries in the data change over time? No
+library(dplyr)
+gapminder %>% group_by(year) %>% summarise(n_distinct(country))
+
+
+
+##### Exercise: Selecting and Filtering
+# Show observations where life expectancy is greater than 80.
+
+
+# Show only population and GDP per capita for Kenya for years before 1970.
+# Show the observation that has the maximum life expectancy
