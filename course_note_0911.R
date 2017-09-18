b968b611e146b2b3c6a3806b5c396873fc3d84bf
Johnny Chiu <johnnychiu@u.northwestern.edu>
2017-09-18 16:48:43 -0500

    backup everything

--- course_note_0911.R
+++ course_note_0911.R
@@ -76,5 +76,54 @@ ggplotly(hc_plot)
   
 # https://github.com/jrnold/ggthemes
      
-    
-                       
+###
+library(tidyverse)
+library(stringr)
+
+x <- c("\"", 
+       "\\", #escape with \,
+       "\t", #change line 
+       "\u00b5") #unicode
+writeLines(x)
+
+str_length(c("R","R for data science"))
+
+fruit <- c("apple","banana",'pear')
+str_sub(fruit, 1, 3)
+str_view(fruit, "an")
+str_view(fruit,".a.") # . : match one and exactly one character
+str_view(c("127.0.0.1"),"\\.") # why do we need two \\?
+str_view(fruit,"^a") # ^ : match the beginning of the string to be "a"
+str_view(fruit,"a$") # $ : match the ending of the string to be "a"
+
+apples <- c('apple pie', 'apple', 'apple cake')
+str_view(apples, "^apple$") # apple to be the beginning, and apple to be the end. It treat apple as a whole.
+
+str_view("127.0.0.1", "\\d\\.\\d")
+str_view_all("127.0.0.1", "\\d\\.\\d")
+
+str_view(c('gray','grey'), "gr[ae]y") # match both a & e for the third character
+str_view(c('gray','grey'), "gr(a|e)y") # match both a & e for the third character
+
+str_view(fruit, "^[^a]") # character class all except a
+
+x <- "MDCCCLXXXVIII" #1888
+str_view(x, "CC+") # match the item that has at least two C's in x , greedy.
+str_view(x, "CC?") # not greedy, ? represent 0 or 1, (not greedy, match the minimum)
+str_view(x, "CC*") # greedy, (greedy, match the maxmimum)
+
+str_view(x, "C[LX]+") # C + LorX greedy
+str_view(c(fruit, "nnaa"),'(na)+') #n have to be followed by a with indefinite times
+
+str_view(x,"C{2}") # exactly 2
+str_view(x, "C{2,}") # 2 or more
+str_view(x, "C{0,2}") # up to 2
+
+str_view(x,"CC*?") # make * not greedy, ? makes * not greedy
+str_view(x,"CC+?") # make * not greedy, ? makes * not greedy
+
+fruit <- c("banana", "coconut", "cucumber", "jujube", "papaya", "salal berry")
+str_view(fruit, "(..)\\1")
+str_view(fruit, "(..){2}")
+
+str_view(c('a a','abc'),"\\s")
